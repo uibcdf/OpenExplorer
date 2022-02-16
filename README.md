01a0f2522848aca6268c175d27cde97b95a430c1
Diego Prada <prada.gracia@gmail.com>
2022-02-15 18:18:28 -0600

    Refact in process

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 # OpenExplorer
+
+OpenExplorer is an open source library to explore the conformational free energy landscape of molecular systems.
+
+## License
+
+This project is under an MIT License. [A copy of the license text is included in this repository](LICENSE).
+
+### Copyright
+
+Copyright (c) 2022 [The Mexico Children's Hospital Federico Gómez](http://himfg.com.mx/), [its Unit of Research on Computational
+Biology and Drug Design](http://uibcdf.org), and [authors](https://github.com/uibcdf/OpenExplorer/graphs/contributors).
+
+## Acknowledgements
+
+Project based on the [Computational Molecular Science Python Cookiecutter](https://github.com/molssi/cookiecutter-cms) version 1.5.
+
+## Contributors
+
+A complete list of contributors can be found checking [the insights section of this
+repository](https://github.com/uibcdf/OpenExplorer/graphs/contributors).
+
+The main project authors and major contributors are:
+
+- Liliana M. Moreno Vargas
+- Diego Prada Gracia
+- Roberto Velázquez Fuentes
+
+
--- setup.py
+++ setup.py
@@ -1,24 +1,47 @@
+"""
+OpenExplorer.
+Python library to explore the conformational free energy landscape of molecular systems.
+"""
+import sys
 from setuptools import setup, find_packages
-from numpy.distutils.core import setup
-from numpy.distutils.extension import Extension
-#import distutils.extension
+import versioneer
 
-extensions_list=[]
+short_description = __doc__.split("\n")
+
+# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:])
+
 
 setup(
     name='openexplorer',
-    version='0.0.1',
     author='UIBCDF Lab',
     author_email='uibcdf@gmail.com',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     package_dir={'openexplorer': 'openexplorer'},
     packages=find_packages(),
-    ext_modules=extensions_list,
-    package_data={'openexplorer': []},
+    include_package_data=True,
+    package_data={'openexplorer': ['data']},
     scripts=[],
+    setup_requires=[] + pytest_runner,
+    platforms=['Linux',
+                'Mac OS-X',
+                'Unix',
+                'Windows',
+    ],
+    python_requires=">=3.7",
     url='http://uibcdf.org',
     download_url ='https://github.com/uibcdf/OpenExplorer',
     license='MIT',
-    description="---",
-    long_description="---",
+    description=short_description[0],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
 
